0408bff326759c3b6e9c80593741f48ed8f9b7b2
David Ezenwa <159185092+david-ezenwa@users.noreply.github.com>
2024-06-16 20:12:08 +0000

    created a web page that calculates the volume of a sphere

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume.
+ */
+function calculateVolumeOfSphere() {
+  // input
+  const radius = parseFloat(document.getElementById('radius').value)
+
+
+  // process
+  const volumeOfPyramid = 4 / 3 * Math.PI * radius ** 3
+
+  // output
+  document.getElementById("volume").innerHTML = 'Volume is: ' + volumeOfPyramid.toFixed(2) + ' mm<sup>3'
+}
